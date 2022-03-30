2fd72fbb1f87deb175bdbbd5a866f117b532d0ec
PHP215 <christopherrobbins031@gmail.com>
2022-03-31 00:31:41 +0100

    notification functionality

--- app/Http/Controllers/notify.php
+++ app/Http/Controllers/notify.php
@@ -7,12 +7,12 @@ use Illuminate\Http\Request;
 class notify extends Controller
 {
     //
-    public function notify(){
+    public function notifier(){
         $details = [
             'body' => 'This is the body of the nofication',
             'text' => 'This is the text of the notification',
             'url' => url('https;//phpjob.herokuapp.com'),
-            'thank_you'=> 'This is the thank you text for the notification'
+            'thank_you'=> 'This is the thank you text for the notification, you have 14 days to enroll'
         ];
     }
 }
