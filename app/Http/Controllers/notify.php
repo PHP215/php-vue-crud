bd2cbb24e1c49d47607bf54023950d4a27e409cb
PHP215 <christopherrobbins031@gmail.com>
2022-03-31 00:37:25 +0100

    notification functionality

--- app/Http/Controllers/notify.php
+++ app/Http/Controllers/notify.php
@@ -2,17 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Notifications\testnotify;
 use Illuminate\Http\Request;
 
 class notify extends Controller
 {
     //
     public function notifier(){
+        $user = User::first();
         $details = [
             'body' => 'This is the body of the nofication',
             'text' => 'This is the text of the notification',
             'url' => url('https;//phpjob.herokuapp.com'),
             'thank_you'=> 'This is the thank you text for the notification, you have 14 days to enroll'
         ];
+        $user->notify(new testnotify($details));
     }
 }
